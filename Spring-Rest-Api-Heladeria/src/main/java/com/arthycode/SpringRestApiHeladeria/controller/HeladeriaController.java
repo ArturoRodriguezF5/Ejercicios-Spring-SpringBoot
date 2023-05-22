4796d40999976509e550c93fdfa226d443ac940d
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-05-22 13:09:34 -0600

    Coommit para actualizar ejercicio elaborado con Spring Boot, H2 sobre Heeladeria.

--- Spring-Rest-Api-Heladeria/src/main/java/com/arthycode/SpringRestApiHeladeria/controller/HeladeriaController.java
+++ Spring-Rest-Api-Heladeria/src/main/java/com/arthycode/SpringRestApiHeladeria/controller/HeladeriaController.java
@@ -2,14 +2,18 @@ package com.arthycode.SpringRestApiHeladeria.controller;
 
 import com.arthycode.SpringRestApiHeladeria.entity.Helado;
 import com.arthycode.SpringRestApiHeladeria.service.HeladeriaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/helado/api/v1")
+@CrossOrigin("*")
 public class HeladeriaController {
     @Autowired
     HeladeriaService heladeriaService;
@@ -17,8 +21,22 @@ public class HeladeriaController {
     public HeladeriaController(HeladeriaService heladeriaService) {
         this.heladeriaService = heladeriaService;
     }
-    @GetMapping("/api/healados")
+    @GetMapping(value = "/all")
+    @Operation(summary = "Returns information about iceCream List.")
     public ResponseEntity<List<Helado>> findAllHel() {
         return heladeriaService.findAll();
     }
+
+    @GetMapping("/find/{id}")
+    @Operation(summary = "Returns information about iceCream of Id.")
+    public ResponseEntity<Optional<Helado>> findOneById(@Parameter(description = "Type Long") @PathVariable Long id) {
+        return heladeriaService.findById(id);
+    }
+
+    @PostMapping(value = "/save")
+    @Operation(summary = "Add a new Helado in the BD.")
+    public ResponseEntity<Helado> addNew(@Parameter(description = "Helado Parameter") @RequestBody Helado helado) {
+        return heladeriaService.addNew(helado);
+    }
+
 }
--- Spring-Rest-Api-Heladeria/src/main/java/com/arthycode/SpringRestApiHeladeria/service/HeladeriaService.java
+++ Spring-Rest-Api-Heladeria/src/main/java/com/arthycode/SpringRestApiHeladeria/service/HeladeriaService.java
@@ -2,13 +2,17 @@ package com.arthycode.SpringRestApiHeladeria.service;
 
 import com.arthycode.SpringRestApiHeladeria.entity.Helado;
 import com.arthycode.SpringRestApiHeladeria.repository.HeladeriaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HeladeriaService {
+    Logger log = LoggerFactory.getLogger(HeladeriaService.class);
     @Autowired
     HeladeriaRepository heladeriaRepository;
 
@@ -17,6 +21,26 @@ public class HeladeriaService {
     }
 
     public ResponseEntity<List<Helado>> findAll() {
+
+        if (heladeriaRepository.count() == 0) {
+            log.warn("List is Empty.");
+            return ResponseEntity.noContent().build();
+        }
         return ResponseEntity.ok(heladeriaRepository.findAll());
+    }
+
+    public ResponseEntity<Optional<Helado>> findById(Long id) {
+        if (id == null) {
+            log.warn("Id is null.");
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(heladeriaRepository.findById(id));
+    }
+
+    public ResponseEntity<Helado> addNew(Helado helado) {
+        if (helado.getId() == null) {
+            return ResponseEntity.ok(heladeriaRepository.save(helado));
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
